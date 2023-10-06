f9b16d6d942fca348edf2478d55af0c29419c8f4
evantianx <evantianx@gmail.com>
2023-10-05 19:57:50 -0600

    feat: init eww with eslint, prettier, husky and commitlint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "eslint-config-prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "eslint-plugin-prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "no-case-declarations": "off",
+    "no-constant-condition": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+  },
+};
